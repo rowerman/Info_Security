5d5d1694036ec0b4478a86ea14623d73ecabc1e3
rowerman <rowerman@sjtu.edu.cn>
2023-12-19 10:49:04 +0800

    first change

--- weibo-search/weibo/UI/ui.py
+++ weibo-search/weibo/UI/ui.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-from tkinter import messagebox
+from tkinter import messagebox, font
 import importlib
 import sys
 import re
@@ -15,34 +15,84 @@ def save_settings():
 
     with open('../settings.py', 'w', encoding='utf-8') as f:
         for line in lines:
-            if line.startswith('DOWNLOAD_DELAY'):
-                f.write('DOWNLOAD_DELAY = {}\n'.format(entry1.get()))
-            elif line.startswith('KEYWORD_LIST'):
-                cookie = '    \'cookie\': \'{}\''.format(entry2.get())
-                f.write(line.replace('    \'cookie\': \'PC_TOKEN=67bb3d0d4e; login_sid_t=e543e272edcee7d8227260f2cffb83ee; cross_origin_proto=SSL; _s_tentry=weibo.com; Apache=2846898638827.7886.1702883046242; SINAGLOBAL=2846898638827.7886.1702883046242; ULV=1702883046244:1:1:1:2846898638827.7886.1702883046242:; SUBP=0033WrSXqPxfM725Ws9jqgMF55529P9D9WFik.OrxkOD-XU2KCZ9GUY.5JpX5o275NHD95QcSKBRS0M7eon7Ws4DqcjMi--NiK.Xi-2Ri--ciKnRi-zNSo-X1hMNehzRe5tt; SSOLoginState=1702883076; SUB=_2A25Ie59VDeRhGeBL71oW9y7OwzyIHXVr-J6drDV8PUNbmtANLVj5kW9NRxyi9I0zrEio6OnBxCh5b2j37yPD8oza; ALF=1734419076\'',cookie))
-            else:
-                f.write(line)
+            if 'DOWNLOAD_DELAY' in line:
+                line = 'DOWNLOAD_DELAY = {}\n'.format(DOWNLOAD_DELAY.get())
+            elif 'DEFAULT_REQUEST_HEADERS' in line and 'cookie' in line:
+                line = re.sub(r"'cookie': '.*?'", "'cookie': '{}'".format(cookie.get()), line)
+            for pipeline in pipelines:  # Change here, loop over all pipelines, not just the ones with entries
+                if pipeline in line:
+                    if var_dict[pipeline].get():
+                        entry = next((e for p, e in pipeline_entries if p == pipeline), None)
+                        priority = '    \'{}\': {}'.format(pipeline, entry.get() if entry else 1)
+                        line = re.sub(r"#?\s*'{}': \d+".format(pipeline), priority, line)
+                    else:
+                        line = re.sub(r"#?\s*'{}': \d+".format(pipeline), '#    \'{}\': 1'.format(pipeline), line)
+            f.write(line)
 
+    importlib.reload(settings)  # Reload the settings module
     messagebox.showinfo("Info", "Settings saved")
+
+
+
 
 root = tk.Tk()
 root.title("Settings")
 root.geometry("900x500")
+my_font = font.Font(family="Arial", size=14)
 
-
-label1 = tk.Label(root, text="DOWNLOAD_DELAY")
-label1.pack()
-entry1 = tk.Entry(root)
+DOWNLOAD_DELAY_frame = tk.Frame(root)
+DOWNLOAD_DELAY_frame.grid(row=0, column=0, sticky="ew")
+DOWNLOAD_DELAY_label = tk.Label(DOWNLOAD_DELAY_frame, text="DOWNLOAD_DELAY:", font=my_font)
+DOWNLOAD_DELAY_label.pack(side="left")
+DOWNLOAD_DELAY = tk.StringVar()
+entry1 = tk.Entry(DOWNLOAD_DELAY_frame,textvariable=DOWNLOAD_DELAY,font=my_font)
 entry1.insert(0, settings.DOWNLOAD_DELAY)  # Set the default value
-entry1.pack()
+entry1.pack(side="left", fill="x", expand=True)
 
-label2 = tk.Label(root, text="cookie")
-label2.pack()
-entry2 = tk.Entry(root)
+cookie_frame = tk.Frame(root)
+cookie_frame.grid(row=0, column=1, sticky="ew")
+cookie_label = tk.Label(cookie_frame, text="cookie:", font=my_font)
+cookie_label.pack(side="left")
+cookie = tk.StringVar()
+entry2 = tk.Entry(cookie_frame,textvariable=cookie,font=my_font)
 entry2.insert(0, settings.DEFAULT_REQUEST_HEADERS['cookie'])  # Set the default value
-entry2.pack()
+entry2.pack(side="left", fill="x", expand=True)
+
+pipelines = [
+    'weibo.pipelines.CsvPipeline',
+    'weibo.pipelines.MysqlPipeline',
+    'weibo.pipelines.MongoPipeline',
+    'weibo.pipelines.MyImagesPipeline',
+    # 'weibo.pipelines.MyVideoPipeline'
+]
+
+pipeline_entries = []
+row = 3
+var_dict = {}
+
+for pipeline in pipelines:
+    var = tk.IntVar()
+    checkbox = tk.Checkbutton(root, text=pipeline, variable=var,font=my_font)
+    checkbox.grid(row=row, column=0, sticky="w")
+
+    entry = tk.Entry(root, font=my_font)
+    entry.grid(row=row, column=1, sticky="ew")
+    entry.grid_remove()  # Initially hide the entry
+
+    def show_entry(var=var, pipeline=pipeline, entry=entry):  # Use default arguments to capture the current pipeline and entry
+        if var.get():
+            entry.grid()  # Show the entry when the checkbox is checked
+            pipeline_entries.append((pipeline, entry))
+        else:
+            entry.grid_remove()  # Hide the entry when the checkbox is unchecked
+            if (pipeline, entry) in pipeline_entries:
+                pipeline_entries.remove((pipeline, entry))
+
+    checkbox.config(command=show_entry)
+    var_dict[pipeline] = var
+    row += 1
 
 button = tk.Button(root, text="Save", command=save_settings)
-button.pack()
+button.grid(row=row, column=0, columnspan=2)
 
 root.mainloop()
--- weibo-search/weibo/settings.py
+++ weibo-search/weibo/settings.py
@@ -18,9 +18,9 @@ ITEM_PIPELINES = {
     'weibo.pipelines.DuplicatesPipeline': 300,
     'weibo.pipelines.CsvPipeline': 301,
     'weibo.pipelines.MysqlPipeline': 302,
-    # 'weibo.pipelines.MongoPipeline': 303,
-    # 'weibo.pipelines.MyImagesPipeline': 304,
-    # 'weibo.pipelines.MyVideoPipeline': 305
+    'weibo.pipelines.MongoPipeline': 303,
+    'weibo.pipelines.MyImagesPipeline': 304,
+    # 'weibo.pipelines.MyVideoPipeline':
 }
 # 要搜索的关键词列表，可写多个, 值可以是由关键词或话题组成的列表，也可以是包含关键词的txt文件路径，
 # 如'keyword_list.txt'，txt文件中每个关键词占一行
