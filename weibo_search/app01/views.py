527ce7630ed90211f0eb2fa1c642bc3da8aead4b
zth855 <133193022+zth855@users.noreply.github.com>
2023-12-25 19:05:32 +0800

    前端页面修改
    修改了html文件和view.py

--- weibo_search/app01/views.py
+++ weibo_search/app01/views.py
@@ -1,12 +1,20 @@
+from django.db.models import Count
+from django.db.models.functions import TruncDate
 from django.shortcuts import render, HttpResponse
 from .models import YourModel
 
 def info_list(request):
-    # table_name = request.GET.get('table_name')  # 从前端获取表名
-    table_name = '甘肃'
     data = YourModel.objects.all()  # 使用指定的数据库查询数据
 
-    return render(request, 'info_list.html', {'data': data})
+    daily_counts = (
+        data
+        .annotate(date=TruncDate('created_at'))
+        .values('date')
+        .annotate(count=Count('id'))
+        .order_by('date')
+    )
+
+    return render(request, 'info_list.html', {'data': data, 'daily_counts': daily_counts})
 
 
 
