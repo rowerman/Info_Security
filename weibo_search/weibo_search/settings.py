17c8bf50a70906037b8d125eb732123b63aa4bab
zth855 <133193022+zth855@users.noreply.github.com>
2023-12-26 18:38:26 +0800

    前端终稿
    导入了新的文件: .\Info_Security\weibo_search\static\china.json，weibo_search\weibo_search\settings.py修改了静态链接的一些内容

--- weibo_search/weibo_search/settings.py
+++ weibo_search/weibo_search/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
@@ -81,7 +81,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.mysql",
         "NAME": "weibo",
         "USER": "root",
-        "PASSWORD": "123456",
+        "PASSWORD": "ZTH7452135",
         "HOST": "127.0.0.1",
         "PORT": "3306",
     }
@@ -122,7 +122,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
